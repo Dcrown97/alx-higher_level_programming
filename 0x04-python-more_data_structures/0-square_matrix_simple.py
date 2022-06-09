1290e788af2c04dbcddff5a0c132531563109d3a
Dcrown <aisimiyuoluwadara@gmail.com>
2022-06-09 11:52:36 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
+    new_matrix = []
 
-        new_matrix = []
+    for num in matrix:
+        new_matrix.append(list(map(lambda x: x**2, num)))
 
-
-
-            for num in matrix:
-
-                        new_matrix.append(list(map(lambda x: x**2, num)))
-
-
-
-                            return new_matrix
+    return new_matrix
